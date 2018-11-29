1c06043d9064075cdaadf2460224aa87ee2c74bf
Otavio Salvador <otavio@ossystems.com.br>
2018-11-29 17:00:26 -0200

    Bump version to 1.0.1
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='hydra-sdk',
     description='Go Hydra SDK for Python',
     keywords='hydra oauth2 openid go',
-    version='1.0.0',
+    version='1.0.1',
     packages=['hydra'],
     install_requires=[
         'requests==2.20.1',
