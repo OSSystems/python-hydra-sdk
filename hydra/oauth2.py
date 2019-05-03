1efaf3302ca27f4d5f51a9ac65a0b6479362bafb
Wouter Westendorp <wouter@gtnetworks.nl>
2019-05-03 15:54:20 +0200

    fix pep-8 errors

--- hydra/oauth2.py
+++ hydra/oauth2.py
@@ -70,53 +70,61 @@ class Client:
 
     def get_login_request(self, challenge):
         response = self.request(
-            'GET', '/oauth2/auth/requests/login', params={'login_challenge': challenge})
+            'GET', '/oauth2/auth/requests/login', 
+            params={'login_challenge': challenge})
         if response.ok:
             return response.json()
 
     def get_consent_request(self, challenge):
         response = self.request(
-            'GET', '/oauth2/auth/requests/consent', params={'consent_challenge': challenge})
+            'GET', '/oauth2/auth/requests/consent', 
+            params={'consent_challenge': challenge})
         if response.ok:
             return response.json()
 
     def accept_login_request(self, challenge, accept_login_config):
         response = self.request(
-            'PUT', '/oauth2/auth/requests/login/accept', params={'login_challenge': challenge},
+            'PUT', '/oauth2/auth/requests/login/accept', 
+            params={'login_challenge': challenge},
             json=accept_login_config)
         if response.ok:
             return response.json()
 
     def accept_consent_request(self, challenge, accept_consent_config):
         response = self.request(
-            'PUT', '/oauth2/auth/requests/consent/accept', params={'consent_challenge': challenge},
+            'PUT', '/oauth2/auth/requests/consent/accept', 
+            params={'consent_challenge': challenge},
             json=accept_consent_config)
         if response.ok:
             return response.json()
 
     def reject_login_request(self, challenge, reject_login_config):
         response = self.request(
-            'PUT', '/oauth2/auth/requests/login/reject', params={'login_challenge': challenge},
+            'PUT', '/oauth2/auth/requests/login/reject', 
+            params={'login_challenge': challenge},
             json=reject_login_config)
         if response.ok:
             return response.json()
 
     def reject_consent_request(self, challenge, reject_consent_config):
         response = self.request(
-            'PUT', '/oauth2/auth/requests/consent/reject', params={'consent_challenge': challenge},
+            'PUT', '/oauth2/auth/requests/consent/reject', 
+            params={'consent_challenge': challenge},
             json=reject_consent_config)
         if response.ok:
             return response.json()
 
     def revokes_all_previous_consent_session_user(self, user):
         response = self.request(
-            'DELETE', '/oauth2/auth/sessions/consent', params={'subject': user})
+            'DELETE', '/oauth2/auth/sessions/consent', 
+            params={'subject': user})
         if response.ok:
             response.json()
 
     def revokes_consent_sessions_oAuth2_client(self, user, client):
         response = self.request(
-            'DELETE', '/oauth2/auth/sessions/consent', params={'subject': user, 'client': client})
+            'DELETE', '/oauth2/auth/sessions/consent', 
+            params={'subject': user, 'client': client})
         return response.ok
 
     def lists_all_consent_sessions_user(self, user):
